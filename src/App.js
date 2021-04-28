e6e96af851aa46f0f0e604ce04e93502d9905d57
RahulGaur998 <gaur.rahul996@gmail.com>
2021-04-28 15:02:26 +0530

    Update with routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import  Homepage  from './pages/Homepage/Homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+); 
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/Hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
